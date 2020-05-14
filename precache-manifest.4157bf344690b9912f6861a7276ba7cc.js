11e03a3e34beb7587a493f0433ea3f4a4decf1dc
drMohamedElkot <dr.m.a.elkot@gmail.com>
2020-05-15 00:43:53 +0300

    Updates

---
+++ precache-manifest.4157bf344690b9912f6861a7276ba7cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20a122dce1c7e4fb11a4d7d4dc2deab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed499c42949eb812a26e",
+    "url": "/static/css/main.f50b1069.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed499c42949eb812a26e",
+    "url": "/static/js/main.17c1d594.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "5996d085e9b0ac2809abf28a22149c4c",
+    "url": "/static/media/logo.5996d085.png"
+  }
+]);
